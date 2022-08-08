1eabd9400dc9339eb59f2a88ede694931a4e4bb1
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2022-08-08 03:51:57 +0000

    Update app/Http/Controllers/Api/AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Response;
 
 class AuthController extends Controller
 {
